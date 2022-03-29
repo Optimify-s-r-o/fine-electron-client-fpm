1cfa94a49e42967029a96902f9a5b5b0942e0692
Richard Pecha <pecha.richard@oasa.ai>
2022-03-29 10:42:09 +0200

    add electron store

--- public/preload.js
+++ public/preload.js
@@ -4,7 +4,7 @@ const keytar = require('keytar');
 contextBridge.exposeInMainWorld('API', {
     openWebBrowser: (url) => shell.openExternal(url),
     on: (eventName, callback) => ipcRenderer.on(eventName, callback),
-    invoke: async (eventName) => ipcRenderer.invoke(eventName),
+    invoke: async (eventName, args) => ipcRenderer.invoke(eventName, args),
     keytarSetSecret: (name, secret) => keytar.setPassword('fpm', name, secret),
     keytarGetSecret: (name) => keytar.getPassword('fpm', name),
 })
--- src/modules/Portal/Settings/System/index.tsx
+++ src/modules/Portal/Settings/System/index.tsx
@@ -3,9 +3,20 @@ import {useTranslation} from "react-i18next";
 import {MainWrapper} from "../../components/Main/components/MainWrapper";
 import * as S from "../../components/Main/styled";
 import {faCog} from "@fortawesome/pro-duotone-svg-icons";
+import {useEffectAsync} from "../../../../utils/useEffectAsync";
 
 const System = () => {
     const {t} = useTranslation(['portal', 'form', 'common']);
+
+    useEffectAsync(async () => {
+        const downloadsPath = await window.API.invoke("ELECTRON_STORE_GET", {name: "downloads"});
+        const documentsPath = await window.API.invoke("ELECTRON_STORE_GET", {name: "documents"});
+        const test = await window.API.invoke("ELECTRON_STORE_GET", {name: "test"});
+        await window.API.invoke("ELECTRON_STORE_SET", {name: "test", value: "C://"});
+        console.log(downloadsPath)
+        console.log(documentsPath)
+        console.log(test)
+    }, []);
 
     return (<MainWrapper
         icon={faCog}

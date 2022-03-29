d144eebb0b8cd0623a71a1b0cdea27fba3dbf899
Karel Křesťan <krestan@optimify.cz>
2022-03-29 20:05:18 +0200

    warning fix

--- src/modules/portal/admin/applicationsSettings/index.tsx
+++ src/modules/portal/admin/applicationsSettings/index.tsx
@@ -3,7 +3,6 @@ import { faDatabase } from '@fortawesome/pro-solid-svg-icons';
 import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
 import { Button } from 'components/Form/Button';
 import { RoutesPath } from 'constants/routes';
-import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import * as Yup from 'yup';

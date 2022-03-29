e23f7f2c72d57b58a6e528a307b1c3980b675867
Karel Křesťan <krestan@optimify.cz>
2022-03-29 17:32:42 +0200

    Edit job

--- src/modules/portal/components/main/components/Navigation.tsx
+++ src/modules/portal/components/main/components/Navigation.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { IconDefinition } from '@fortawesome/pro-solid-svg-icons';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { IconDefinition } from '@fortawesome/pro-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { RoutesPath } from '../../../../../constants/routes';
+import styled from 'styled-components';
 
 export type NavigationItem = {
-  path: RoutesPath;
+  path: string;
   active: boolean;
   text: string;
   icon?: IconDefinition;
   isAction?: boolean;
 };
 
-export const Navigation = ({ items, title }: { items: NavigationItem[]; title?: string }) => {
+export const Navigation = ( { items, title }: { items: NavigationItem[]; title?: string; } ) => {
   return (
     <NavigationWrapper>
       {title && <Title>{title}</Title>}
-      {items?.map((item: NavigationItem, key: number) => {
+      {items?.map( ( item: NavigationItem, key: number ) => {
         return (
           <Link to={item.path} key={key}>
             <Item active={item.active ? 1 : 0} isAction={item.isAction}>
@@ -27,7 +26,7 @@ export const Navigation = ({ items, title }: { items: NavigationItem[]; title?: 
             </Item>
           </Link>
         );
-      })}
+      } )}
     </NavigationWrapper>
   );
 };
@@ -35,53 +34,53 @@ export const Navigation = ({ items, title }: { items: NavigationItem[]; title?: 
 const Title = styled.div`
   padding: 8px 24px;
 
-  color: ${(props) => props.theme.common.content};
+  color: ${ ( props ) => props.theme.common.content };
 `;
 
-const Item = styled.div<{ active: number; isAction?: boolean }>`
-  padding: ${(props) => (props.active ? '10px' : '8px')} 24px
-    ${(props) => (props.active ? '10px' : '')};
+const Item = styled.div<{ active: number; isAction?: boolean; }>`
+  padding: ${ ( props ) => ( props.active ? '10px' : '8px' ) } 24px
+    ${ ( props ) => ( props.active ? '10px' : '' ) };
 
-  background-color: ${(props) =>
+  background-color: ${ ( props ) =>
     props.active
       ? props.theme.common.contentSecondary + ' !important'
       : props.isAction
-      ? 'transparent !important'
-      : props.theme.colors.background.menu};
-  color: ${(props) =>
+        ? 'transparent !important'
+        : props.theme.colors.background.menu };
+  color: ${ ( props ) =>
     props.active
       ? props.theme.colors.primary.default
       : props.isAction
-      ? props.theme.common.content
-      : props.theme.colors.secondaryText.default};
+        ? props.theme.common.content
+        : props.theme.colors.secondaryText.default };
 
   svg {
     margin-right: 4px;
-    color: ${(props) =>
-      props.active ? props.theme.colors.primary.default : props.theme.common.darker};
+    color: ${ ( props ) =>
+    props.active ? props.theme.colors.primary.default : props.theme.common.darker };
   }
 
   &:hover {
-    background-color: ${(props) =>
-      props.active
-        ? props.theme.common.contentSecondary
-        : props.isAction
+    background-color: ${ ( props ) =>
+    props.active
+      ? props.theme.common.contentSecondary
+      : props.isAction
         ? 'rgba(255, 255, 255, 0.05)'
-        : props.theme.colors.background.content} !important;
-    color: ${(props) =>
-      props.active
-        ? props.theme.colors.primary.default
-        : props.isAction
+        : props.theme.colors.background.content } !important;
+    color: ${ ( props ) =>
+    props.active
+      ? props.theme.colors.primary.default
+      : props.isAction
         ? props.theme.colors.background.content
-        : props.theme.colors.secondaryText.hover};
+        : props.theme.colors.secondaryText.hover };
 
     svg {
-      color: ${(props) =>
-        props.active
-          ? props.theme.colors.primary.default
-          : props.isAction
-          ? props.theme.common.content
-          : props.theme.colors.secondaryText.default};
+      color: ${ ( props ) =>
+    props.active
+      ? props.theme.colors.primary.default
+      : props.isAction
+        ? props.theme.common.content
+        : props.theme.colors.secondaryText.default };
     }
   }
 `;
@@ -97,8 +96,8 @@ const NavigationWrapper = styled.div`
   > a {
     text-decoration: none;
 
-    &:nth-child(even) ${Item} {
-      background-color: ${(props) => props.theme.colors.background.secondaryMenu};
+    &:nth-child(even) ${ Item } {
+      background-color: ${ ( props ) => props.theme.colors.background.secondaryMenu };
     }
   }
 `;
--- src/modules/portal/projects/editProject/index.tsx
+++ src/modules/portal/projects/editProject/index.tsx
@@ -1,48 +1,104 @@
 import { faFolder } from '@fortawesome/pro-light-svg-icons';
 import { faDatabase } from '@fortawesome/pro-solid-svg-icons';
 import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
-import { ProjectCreateRequest } from 'api/generated';
+import { ProjectCreateRequest, ProjectDto } from 'api/generated';
 import { Button } from 'components/Form/Button';
 import { RoutesPath } from 'constants/routes';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { useParams } from 'react-router-dom';
+import API from 'utils/api';
+import { useApi } from 'utils/hooks/useApi';
 import * as Yup from 'yup';
 
+import { JobDto, ProjectJobsDto } from '../../../../api/generated/api';
+import { useEffectAsync } from '../../../../utils/useEffectAsync';
 import { MainWrapper } from '../../components/main/components/MainWrapper';
 import * as S from '../../components/main/styled';
 
 const EditProject = () => {
-  const { t } = useTranslation(['portal', 'form', 'common']);
+  const { editId } = useParams();
+  const { t } = useTranslation( ['portal', 'form', 'common'] );
 
-  const { handleSubmit } = useForm<ProjectCreateRequest>({
+  const [getProjectMain, { loading: mainLoading }] = useApi<ProjectDto>();
+  const [getProjectJobs, { loading: jobsLoading }] = useApi<ProjectJobsDto>();
+
+  const [mainData, setMainData] = useState<ProjectDto | null>( null );
+  const [jobsData, setJobsData] = useState<JobDto[]>( [] );
+
+  const { handleSubmit } = useForm<ProjectCreateRequest>( {
     resolver: yupResolver(
-      Yup.object().shape({
-        name: Yup.string().required(t('form:validation.required'))
-      })
+      Yup.object().shape( {
+        name: Yup.string().required( t( 'form:validation.required' ) )
+      } )
     )
-  });
+  } );
 
-  const onSubmit = async (data: ProjectCreateRequest) => {
-    console.log(data);
+  const onSubmit = async ( data: ProjectCreateRequest ) => {
+    console.log( data );
   };
+
+  //Load everything during window open
+  useEffectAsync( async () => {
+    if ( editId ) {
+      const res = await getProjectMain( () => API.ProjectsApi.fineProjectManagerApiProjectsIdGet( editId ) );
+      setMainData( res );
+    }
+  }, [editId] );
+
+  useEffectAsync( async () => {
+    if ( editId ) {
+      const res = await getProjectJobs( () => API.ProjectsApi.fineProjectManagerApiProjectsIdJobsGet( editId ) );
+      setJobsData( res.jobs );
+    }
+  }, [editId] );
 
   return (
     <MainWrapper
       icon={faFolder}
-      title={t('portal:menu.editProject')}
+      title={mainData ? mainData.name : ''}
       navigation={[
         {
-          path: RoutesPath.SYSTEM,
+          path: `${ RoutesPath.PROJECTS }/${ editId }`,
           active: true,
-          text: t('portal:menu.editProject'),
+          text: t( 'portal:projects.tabs.editProjectMain' ),
+          icon: faDatabase
+        },
+        {
+          path: `${ RoutesPath.PROJECTS }/${ editId }/files`,
+          active: false,
+          text: t( 'portal:projects.tabs.editProjectFiles' ),
           icon: faDatabase
         }
       ]}
     >
-      <S.MainFormContent onSubmit={handleSubmit(onSubmit)}>
-        <S.ContentWrapper>TODO EDIT PROJECT</S.ContentWrapper>
+      <S.MainFormContent onSubmit={handleSubmit( onSubmit )}>
+        <S.ContentWrapper>
+          {mainLoading ? 'loading main' :
+            <div>
+              {'Cast 1 (editovatelna pole, vaze se k nim tlacitko "Ulozit", cili bych ho nedaval uplne dolu, ale nekam pod to):'}
+              {`Nazev: ${ mainData?.name }`}
+              {`Popis: ${ mainData?.description }`}
+            </div>
+          }
+          <div><br />Cast 2: Tabulka se seznamem jobu, pozor ze jich muze byt docela hodne, radove i desitky, jsou to ale jednoducha data, proto nepouzivame pagination</div>
+          {
+            jobsLoading ? 'loading jobs' :
+              <>
+                {/*TODO Karel preklady aplikaci a typu z contextu*/}
+                {jobsData.map( e => <div>{`Nazev: ${ e.name } Popis: ${ e.description } Typ: ${ e.type } Aplikace: ${ e.application } Pridano: ${ e.createdAt }`}</div> )}
+              </>
+          }
+          <div>Ohledne tlacitek v te tabulce: </div>
+          <div>Prvni tlacitko je navigace na {`${ RoutesPath.JOBS }/{id}`}. Ikonu jako chces a je vzdy stejna</div>
+          <div>Druhe tlacitko je spusteni programu. Ted nebudeme implementovat. Zobrazuj, pokud e.IsOpenable. Ikonu si budu muset tahat z ApplicationsContextu, bude to url na obrazek, zatim tam dej prosim fixne url na nejaky obrazek</div>
+          <div>Treti tlacitko vede na stazeni souboru, pridej jenom ikonku a onClickHandler s logem, zbytek si dodelam</div>
+          <div>Posledni tlacitko vyvola modal s dotazem na delete, ten prosim udelej, delete tlacitko si implementuju</div>
+
+        </S.ContentWrapper>
         <S.ButtonsWrapper>
-          <Button loading={false}>{t('form:button.save')}</Button>
+          <Button loading={false}>{t( 'form:button.save' )}</Button>
         </S.ButtonsWrapper>
       </S.MainFormContent>
     </MainWrapper>

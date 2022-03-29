70f9a3676b087f031cb2dcd3dd97d0e53e738762
Karel Křesťan <krestan@optimify.cz>
2022-03-29 13:22:19 +0200

    translation

--- src/modules/Portal/Projects/CreateProject/index.tsx
+++ src/modules/Portal/Projects/CreateProject/index.tsx
@@ -1,43 +1,44 @@
-import {faFolder} from "@fortawesome/pro-light-svg-icons";
-import {RoutesPath} from "constants/routes";
-import {faDatabase} from "@fortawesome/pro-solid-svg-icons";
-import {Button} from "components/Form/Button";
-import {useTranslation} from "react-i18next";
-import {MainWrapper} from "../../components/Main/components/MainWrapper";
-import * as S from "../../components/Main/styled";
-import {useForm} from "react-hook-form";
-import {yupResolver} from "@hookform/resolvers/yup/dist/yup";
-import * as Yup from "yup";
-import {CreateProjectForm} from "./components/Form";
-import {ProjectCreateRequest} from "api/generated";
+import { faFolder } from '@fortawesome/pro-light-svg-icons';
+import { faDatabase } from '@fortawesome/pro-solid-svg-icons';
+import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
+import { ProjectCreateRequest } from 'api/generated';
+import { Button } from 'components/Form/Button';
+import { RoutesPath } from 'constants/routes';
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import * as Yup from 'yup';
+
+import { MainWrapper } from '../../components/Main/components/MainWrapper';
+import * as S from '../../components/Main/styled';
+import { CreateProjectForm } from './components/Form';
 
 const CreateProject = () => {
-    const {t} = useTranslation(['portal', 'form', 'common']);
+    const { t } = useTranslation( ['portal', 'form', 'common'] );
 
-    const {register, handleSubmit} = useForm<ProjectCreateRequest>({
-        resolver: yupResolver(Yup.object().shape({
+    const { register, handleSubmit } = useForm<ProjectCreateRequest>( {
+        resolver: yupResolver( Yup.object().shape( {
             name: Yup.string()
-                .required(t("form:validation.required")),
-        })),
-    });
+                .required( t( "form:validation.required" ) ),
+        } ) ),
+    } );
 
-    const onSubmit = async (data: ProjectCreateRequest) => {
-        console.log(data);
-    }
+    const onSubmit = async ( data: ProjectCreateRequest ) => {
+        console.log( data );
+    };
 
-    return (<MainWrapper
+    return ( <MainWrapper
         icon={faFolder}
-        title={t("portal:menu.createProject")}
+        title={t( "portal:menu.createProject" )}
         navigation={[{
-            path: RoutesPath.SYSTEM, active: true, text: "test", icon: faDatabase,
+            path: RoutesPath.SYSTEM, active: true, text: t( "portal:projects.tabs.newProject" ), icon: faDatabase,
         },]}>
-        <S.MainFormContent onSubmit={handleSubmit(onSubmit)}>
+        <S.MainFormContent onSubmit={handleSubmit( onSubmit )}>
             <S.ContentWrapper>
-                <CreateProjectForm register={register}/>
+                <CreateProjectForm register={register} />
             </S.ContentWrapper>
-            <S.ButtonsWrapper><Button loading={false}>{t("form:button.createProject")}</Button></S.ButtonsWrapper>
+            <S.ButtonsWrapper><Button loading={false}>{t( "form:button.createProject" )}</Button></S.ButtonsWrapper>
         </S.MainFormContent>
-    </MainWrapper>)
-}
+    </MainWrapper> );
+};
 
 export default CreateProject;

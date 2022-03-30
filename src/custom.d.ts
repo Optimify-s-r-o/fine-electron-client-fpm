86eba5e27a1c168570ae27d17cec67e6275a3f94
Karel Křesťan <krestan@optimify.cz>
2022-03-30 18:33:24 +0200

    keytar fix

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,8 +1,7 @@
-import keytar from 'keytar';
 import { execFile } from 'child_process';
 
 declare module '*.svg' {
-  import React = require('react');
+  import React = require( 'react' );
   export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
   const src: string;
   export default src;
@@ -16,12 +15,13 @@ declare module '*.png' {
 //TODO improve typing
 export interface IElectronAPI {
   fs: any;
-  execFile: (filePath, args) => any;
-  openWebBrowser: (url: string) => Promise<void>;
-  on: (eventName, callback) => any;
-  invoke: (eventName: any, args?: any) => Promise<any>;
-  keytarSetSecret: (name, secret) => Promise<void>;
-  keytarGetSecret: (name) => Promise<string | null>;
+  execFile: ( filePath, args ) => any;
+  openWebBrowser: ( url: string ) => Promise<void>;
+  on: ( eventName, callback ) => any;
+  invoke: ( eventName: any, args?: any ) => Promise<any>;
+  keytarSetSecret: ( name, secret ) => Promise<void>;
+  keytarGetSecret: ( name ) => Promise<string | null>;
+  keytarDeleteSecret: ( name ) => Promise<void>,
 }
 
 declare global {
--- src/modules/auth/context/AuthProvider.tsx
+++ src/modules/auth/context/AuthProvider.tsx
@@ -1,89 +1,89 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
 import { config } from 'utils/api';
 import { useApi } from 'utils/hooks/useApi';
 
 import { SignInRequest, SignInResponse, UserDto } from '../../../api/generated/api';
+import { RoutesPath } from '../../../constants/routes';
 import API from '../../../utils/api';
 import { AuthContext } from './AuthContext';
-import { useNavigate } from 'react-router';
-import { RoutesPath } from '../../../constants/routes';
 
 // TODO token do local storage
-export const AuthProvider = ({ children }: { children: JSX.Element }) => {
-  const [token, setToken] = useState<string | null>(null);
-  const [user, setUser] = useState<UserDto | null>(null);
-  const [validityEnd, setValidityEnd] = useState<Date | undefined>(undefined);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [isLogged, setIsLogged] = useState<boolean>(user !== null);
+export const AuthProvider = ( { children }: { children: JSX.Element; } ) => {
+  const [token, setToken] = useState<string | null>( null );
+  const [user, setUser] = useState<UserDto | null>( null );
+  const [validityEnd, setValidityEnd] = useState<Date | undefined>( undefined );
+  const [isLoading, setIsLoading] = useState<boolean>( false );
+  const [isLogged, setIsLogged] = useState<boolean>( user !== null );
 
   const [signIn, { loading: signInLoading }] = useApi<SignInResponse, SignInRequest>();
   const [fetchSelf, { loading: fetchLoading }] = useApi<UserDto>();
-  const [loading, setLoading] = useState<boolean>(signInLoading || fetchLoading);
+  const [loading, setLoading] = useState<boolean>( signInLoading || fetchLoading );
 
   const navigate = useNavigate();
 
-  useEffect(() => {
-    setLoading(signInLoading || fetchLoading);
-  }, [signInLoading, fetchLoading]);
+  useEffect( () => {
+    setLoading( signInLoading || fetchLoading );
+  }, [signInLoading, fetchLoading] );
 
   const IsSavedTokenValid = async () => {
-    const token = await window.API.keytarGetSecret('token');
-    const email = await window.API.keytarGetSecret('email');
+    const token = await window.API.keytarGetSecret( 'token' );
+    const email = await window.API.keytarGetSecret( 'email' );
 
-    if (token === null || email === null) return false;
+    if ( token === null || email === null ) return false;
 
     try {
-      const result = await fetchSelf(() => API.UsersApi.fineProjectManagerApiUsersEmailGet(email));
+      const result = await fetchSelf( () => API.UsersApi.fineProjectManagerApiUsersEmailGet( email ) );
 
-      setUser(result);
+      setUser( result );
 
       return true;
     } catch {
-      await window.API.keytarSetSecret('token', null);
+      await window.API.keytarDeleteSecret( 'token' );
       return false;
     }
   };
 
-  const SignIn = async (username: string, password: string) => {
-    setIsLoading(true);
+  const SignIn = async ( username: string, password: string ) => {
+    setIsLoading( true );
 
     try {
-      const result = await signIn(() =>
-        API.UsersApi.fineProjectManagerApiUsersSignInPost({
+      const result = await signIn( () =>
+        API.UsersApi.fineProjectManagerApiUsersSignInPost( {
           email: username,
           password: password
-        })
+        } )
       );
 
-      await window.API.keytarSetSecret('token', result.token);
+      await window.API.keytarSetSecret( 'token', result.token );
 
       config.apiKey = 'Bearer ' + result.token;
-      setToken(result.token as string);
-      setUser(result.user as UserDto);
-      setValidityEnd(new Date(result.expiration as string));
-      setIsLogged(true);
-      setIsLoading(false);
+      setToken( result.token as string );
+      setUser( result.user as UserDto );
+      setValidityEnd( new Date( result.expiration as string ) );
+      setIsLogged( true );
+      setIsLoading( false );
 
       return true;
-    } catch (e) {
-      setIsLoading(false);
+    } catch ( e ) {
+      setIsLoading( false );
       return false;
     }
   };
 
   const SignOut = async () => {
-    setIsLoading(true);
+    setIsLoading( true );
 
     config.apiKey = '';
-    setToken(null);
-    setUser(null);
-    setValidityEnd(undefined);
-    await window.API.keytarSetSecret('token', null);
-    setIsLogged(false);
+    setToken( null );
+    setUser( null );
+    setValidityEnd( undefined );
+    await window.API.keytarDeleteSecret( 'token' );
+    setIsLogged( false );
 
-    setIsLoading(false);
+    setIsLoading( false );
 
-    navigate(RoutesPath.SIGN_IN);
+    navigate( RoutesPath.SIGN_IN );
   };
 
   return (

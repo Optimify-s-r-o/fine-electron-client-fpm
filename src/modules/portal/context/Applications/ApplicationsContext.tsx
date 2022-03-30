f1749f6d5773e1a93f2f76f8bbe0074e8b4058c7
Karel Křesťan <krestan@optimify.cz>
2022-03-30 21:59:23 +0200

    refetch

--- src/modules/portal/context/Applications/ApplicationsContext.tsx
+++ src/modules/portal/context/Applications/ApplicationsContext.tsx
@@ -5,11 +5,13 @@ import { ApplicationDto } from '../../../../api/generated/api';
 interface IApplicationsContextType {
   applications: ApplicationDto[];
   loading: boolean;
+  refetch: () => Promise<void>;
 }
 
 export const ApplicationContext = createContext<IApplicationsContextType>( {
   applications: [],
-  loading: false
+  loading: false,
+  refetch: () => new Promise<void>( e => 'no valid context' )
 } );
 
 export const useApplicationContext = () => useContext( ApplicationContext );
--- src/modules/portal/context/Applications/ApplicationsProvider.tsx
+++ src/modules/portal/context/Applications/ApplicationsProvider.tsx
@@ -7,28 +7,36 @@ import { useEffectAsync } from 'utils/useEffectAsync';
 import { ApplicationDto, ApplicationDtoPaginatedCollection } from '../../../../api/generated/api';
 import { ApplicationContext } from './ApplicationsContext';
 
-export const ApplicationsProvider = ({ children }: { children: JSX.Element }) => {
-  const [applications, setApplications] = useState<ApplicationDto[]>([]);
+export const ApplicationsProvider = ( { children }: { children: JSX.Element; } ) => {
+  const [applications, setApplications] = useState<ApplicationDto[]>( [] );
   const { user, isLogged, loading: userLoading } = useAuthContext();
 
   const [getApplications, { loading: applicationLoading }] =
     useApi<ApplicationDtoPaginatedCollection>();
 
   // Fetch applications during portal startup
-  useEffectAsync(async () => {
-    if (isLogged && !userLoading) {
-      const res = await getApplications(() =>
+  useEffectAsync( async () => {
+    if ( isLogged && !userLoading ) {
+      const res = await getApplications( () =>
         API.ApplicationsApi.fineProjectManagerApiApplicationsNoPaginationGet()
       );
-      setApplications(res.data as ApplicationDto[]);
+      setApplications( res.data as ApplicationDto[] );
     }
-  }, [isLogged, user, userLoading]);
+  }, [isLogged, user, userLoading] );
+
+  const refetch = async () => {
+    const res = await getApplications( () =>
+      API.ApplicationsApi.fineProjectManagerApiApplicationsNoPaginationGet()
+    );
+    setApplications( res.data as ApplicationDto[] );
+  };
 
   return (
     <ApplicationContext.Provider
       value={{
         applications: applications,
-        loading: applicationLoading
+        loading: applicationLoading,
+        refetch: refetch
       }}>
       {children}
     </ApplicationContext.Provider>

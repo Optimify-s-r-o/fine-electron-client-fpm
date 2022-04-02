e8e454747ed988cad8e22b898ff85311891e0f6d
Richard Pecha <pecha.richard@oasa.ai>
2022-04-02 18:37:00 +0200

    use hash router

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,17 @@ import './translations/i18n';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import App from './App';
 import { darkTheme, lightTheme } from './constants/theme';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <ThemeProvider theme={true ? lightTheme : darkTheme}>
       <App />
     </ThemeProvider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );

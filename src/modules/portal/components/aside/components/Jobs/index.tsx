a95657f8de553ddb889ff47f2ac2f821ce827ba7
Karel Křesťan <krestan@optimify.cz>
2022-04-09 20:03:20 +0200

    todo removed

--- src/modules/portal/components/aside/components/Jobs/index.tsx
+++ src/modules/portal/components/aside/components/Jobs/index.tsx
@@ -13,10 +13,6 @@ import API from '../../../../../../utils/api';
 import { useApi } from '../../../../../../utils/hooks/useApi';
 import { Skeleton } from '../Skeleton';
 
-//TODO MARA
-// Strankovani tady neexistuje, vsechny operace se provadi s lokalnimi daty1
-// Sort = Nazev/{Name}, Datum/{UpdatedAt} a Typ/{Application, pak Type}
-// Pozor na to, ze muze byt dlouhy nazev, zkracujme ho aby se vesel na jeden radek
 export const Jobs = () => {
   const { t } = useTranslation(['portal']);
 

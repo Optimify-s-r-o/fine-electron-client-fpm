dbabe682464d101765e25beb1d2c68fc0a714ed7
Karel Křesťan <krestan@optimify.cz>
2022-04-20 20:05:49 +0200

    import job try finish without exe file

--- src/modules/portal/projects/editProject/components/ImportJobData.tsx
+++ src/modules/portal/projects/editProject/components/ImportJobData.tsx
@@ -2,12 +2,15 @@ import { faFileArrowUp } from '@fortawesome/pro-light-svg-icons';
 import { ProjectDto } from 'api/generated';
 import ProgressModalFiles, { ProgressRunFunctionWithFiles } from 'components/Progress/ProgressModalFiles';
 import { useTranslation } from 'react-i18next';
+import API from 'utils/api';
+import { useApi } from 'utils/hooks/useApi';
 import { ProgressStatus } from 'utils/hooks/useProgress';
 
-import { JobCreateRequest } from '../../../../../api/generated/api';
+import { JobCreateRequest, JobDto } from '../../../../../api/generated/api';
 
 export const ImportJobData = ({ project }: { project?: ProjectDto | null }) => {
   const { t } = useTranslation(['portal', 'form', 'common', 'project', 'job']);
+  const [jobCreate] = useApi<JobDto, JobCreateRequest>();
 
   const createFileToWrite = async (inputFile: File) => {
       const response = await window.API.invoke( 'GET_TEMP_DIRECTORY' );
@@ -75,6 +78,7 @@ export const ImportJobData = ({ project }: { project?: ProjectDto | null }) => {
     
         // Do actual work
         for ( let i = 0; i < files.length; i++ ) {
+            // Create file to write
             setItemStatus( i, ProgressStatus.Running, t('job:import.jobRunning') );
             
             const fileRes = await createFileToWrite( files[i] );
@@ -85,12 +89,49 @@ export const ImportJobData = ({ project }: { project?: ProjectDto | null }) => {
                 continue;
             }
 
+            // Handle File Info exe
             const res = await openFileInfoExe((files[i] as any).path, fileRes?.file as string, fileRes?.directory as string);
 
             console.log( res );
-            const statusText = res.exitCode === 0 ? t('job:import.jobSuccess') : res.exitCode === null ? t('job:import.jobFailExeOpen') : t('job:import.jobFailOther')
+            const statusText = res.exitCode === 0 ? t('job:import.jobReadingOutputProgress') : res.exitCode === null ? t('job:import.jobFailExeOpen') : t('job:import.jobFailOther')
+            setItemStatus( i, res.exitCode === 0 ? ProgressStatus.Running : ProgressStatus.Fail, statusText );
 
-            setItemStatus( i, res.exitCode === 0 ? ProgressStatus.Success : ProgressStatus.Fail, statusText );
+            if ( res.exitCode !== 0 ) {
+                continue;
+            }
+
+            // Load output file from
+            const fs = window.API.fs;
+            let fileContent: string;
+
+            try {
+                fileContent = fs.readFileSync( fileRes.directory + '\\' + fileRes.file, { encoding: 'utf-8', flag: 'r' });
+                
+            } catch ( error ) {
+                setItemStatus( i, ProgressStatus.Fail, t( 'job:import.jobFailReadOutput' ) );
+                continue;
+            }
+
+            // Parse output file
+            const parsedContent: JobCreateRequest | undefined = JSON.parse(fileContent);
+
+            if ( !parsedContent ) {
+                setItemStatus( i, ProgressStatus.Fail, t( 'job:import.jobFailReadOutput' ) );
+                continue;
+            }
+
+            // Upload output file
+            setItemStatus( i, ProgressStatus.Running, t( 'job:import.jobUploadingInProgress' ) );
+            
+            try {
+                const res = await jobCreate( () => API.JobsApi.fineProjectManagerApiJobsPost( parsedContent as JobCreateRequest ) );
+                console.log( res );
+                setItemStatus( i, ProgressStatus.Success, t( 'job:import.jobSuccess' ) );
+
+            } catch ( exception ) {
+                console.log( exception );
+                setItemStatus( i, ProgressStatus.Fail, t( 'job:import.jobFailUpload' ) );
+            }
         }
 
         finish();
--- src/utils/jobs/importJobData.ts
+++
@@ -1,4 +0,0 @@
-
-export const ImportJobData = ( files: FileList ) => async () => {
-    for ( let i = 0; i < files.length; ) console.log( 'porno' );
-};
